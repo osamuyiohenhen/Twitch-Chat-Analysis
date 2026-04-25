737bede2a64695f9e1d06f9ccc43a001fbe17ed4
Osamuyi Ohenhen <162273021+osamuyiohenhen@users.noreply.github.com>
2026-04-25 01:50:39 -0400

    Format bot_list

--- config.py
+++ config.py
@@ -5,4 +5,9 @@ load_dotenv()
 
 client_id = os.getenv("TWITCH_CLIENT_ID")
 client_secret = os.getenv("TWITCH_CLIENT_SECRET")
-bot_list = ["fossabot", "nightbot", "streamelements", "potatbotat"] # Add more known bots
+bot_list = [
+    "fossabot",
+    "nightbot",
+    "streamelements",
+    "potatbotat",
+]  # Add more known bots

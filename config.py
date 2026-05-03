52b04422e1b3d273579b1a5e9915bc2bb1365b0f
Osamuyi Ohenhen <162273021+osamuyiohenhen@users.noreply.github.com>
2026-05-03 17:54:55 -0400

    Add user auth tokens and update primary.py

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ load_dotenv()
 
 client_id = os.getenv("TWITCH_CLIENT_ID")
 client_secret = os.getenv("TWITCH_CLIENT_SECRET")
+user_token = os.getenv("TWITCH_USER_TOKEN")
+refresh_token = os.getenv("TWITCH_REFRESH_TOKEN")
+
 bot_list = [
     "fossabot",
     "nightbot",

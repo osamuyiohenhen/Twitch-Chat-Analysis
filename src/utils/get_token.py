e11f2b331ac41847f31ae5385e70d8efcbddd450
Osamuyi Ohenhen <162273021+osamuyiohenhen@users.noreply.github.com>
2026-05-03 18:52:47 -0400

    Reformat, add disclaimer, and update README

--- src/utils/get_token.py
+++ src/utils/get_token.py
@@ -7,11 +7,15 @@ import config
 
 TARGET_SCOPES = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
 
+
 async def main():
-    twitch = await Twitch(config.client_id, config.client_secret, authenticate_app=False)
+    twitch = await Twitch(
+        config.client_id, config.client_secret, authenticate_app=False
+    )
     auth = UserAuthenticator(twitch, TARGET_SCOPES)
     token, refresh_token = await auth.authenticate()
     print(f"Token: {token}")
     print(f"Refresh: {refresh_token}")
+
 
 asyncio.run(main())
